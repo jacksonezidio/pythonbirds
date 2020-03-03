bb03d12f25dde913760f10564700f1f8e5c0dfcf
Jackson E. de Deus <jacksonezidio@gmail.com>
2020-03-03 12:34:13 -0300

    testes classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,22 @@
 class Pessoa:
-    pass
+
+    def __init__(self,nome=None,idade=31):
+        self.nome = nome
+        self.idade = idade
+
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa("Jackson")
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
+
+    p.nome = "Jack"
+    p.idade = 29
+
+    print(p.nome)
+    print(p.idade)
